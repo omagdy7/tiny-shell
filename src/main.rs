ff2be89a59fc78de17d8f5cffa5a8103be83c9b2
omagdy7 <omar.professional8777@gmail.com>
2024-05-31 18:31:44 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,35 @@
 use std::io::{self, Write};
 use std::process::exit;
 
+struct ShellCommand<'a> {
+    cmd: &'a str,
+    args: &'a [&'a str],
+}
+
+impl<'a> From<&'a [&'a str]> for ShellCommand<'a> {
+    fn from(value: &'a [&'a str]) -> Self {
+        ShellCommand {
+            cmd: &value[0],
+            args: &value[1..],
+        }
+    }
+}
+
 fn eval(command: &str) {
+    let v = command.split(' ').collect::<Vec<&str>>();
+    let shell_cmd = ShellCommand::from(v.as_slice());
     if command.split_once(' ').is_some() {
-        let (cmd, args) = command.split_once(' ').unwrap();
-        let args = args.trim_end();
-        let exit_num = args.parse::<i32>().expect("This should be parsable");
+        let (cmd, args) = (shell_cmd.cmd, shell_cmd.args);
         match cmd {
-            "exit" => exit(exit_num),
+            "exit" => {
+                let args = args[0].trim_end();
+                let exit_num = args.parse::<i32>().expect("This should be parsable");
+                exit(exit_num)
+            }
+            "echo" => {
+                let line_to_print = args.join(" ");
+                print!("{}", line_to_print);
+            }
             _ => {}
         }
     } else {
