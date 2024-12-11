0dfa382b710f84da3dc1c4521d61909460c74aaa
omagdy <omar.professional8777@gmail.com>
2024-12-11 16:32:32 +0200

    feat: Added support for Navigation using cd and used proper error handling across the code

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
-#[allow(unused_imports)]
+#![forbid(unsafe_code)] // Disallow all unsafe code
+#![warn(unused_results)] // Warn on unused Result or Option types
+#![warn(clippy::all)] // Enable all Clippy lints
+#![warn(clippy::pedantic)] // Enable strict Clippy lints
+#![warn(clippy::unwrap_used)] // Warn when unwrap or expect are used
+#![warn(clippy::panic)] // Warn on use of panic!
+#![warn(clippy::result_unwrap_used)] // Warn on Result.unwrap()
+#![warn(clippy::option_unwrap_used)] // Warn on Option.unwrap()
+#![warn(clippy::redundant_clone)] // Warn on unnecessary clones
+#![warn(clippy::needless_pass_by_value)] // Suggest borrowing instead of copying
+
+use anyhow::anyhow;
 use std::io::{self, Write};
 use std::{
     collections::HashMap,
     env, fs,
     path::PathBuf,
     process::{exit, Command},
-    str::FromStr,
 };
 
 #[derive(Debug)]
@@ -23,7 +33,7 @@ struct ShellCommand<'a> {
 
 #[derive(Debug)]
 struct Context {
-    executbles: HashMap<String, PathBuf>,
+    executables: HashMap<String, PathBuf>,
     current_working_dir: PathBuf,
 }
 
@@ -31,6 +41,78 @@ fn _listdir(p: &PathBuf) -> Result<Vec<PathBuf>, io::Error> {
     fs::read_dir(p)?
         .map(|res| res.map(|e| e.path()))
         .collect::<Result<Vec<_>, io::Error>>()
+}
+
+fn change_directory(ctx: &mut Context, args: &[&str]) -> Result<(), anyhow::Error> {
+    match args.len() {
+        0 => {
+            // Go to home directory when no arguments are provided
+            ctx.current_working_dir = PathBuf::from(HOME);
+            std::env::set_current_dir(&ctx.current_working_dir)?;
+        }
+        1 => {
+            let path = args[0];
+            let new_path = resolve_path(ctx, path)?;
+
+            // Attempt to change directory and update context
+            if new_path.exists() && new_path.is_dir() {
+                ctx.current_working_dir = new_path.clone();
+                std::env::set_current_dir(&new_path)?;
+            } else {
+                return Err(anyhow!("cd: {}: No such directory", new_path.display()));
+            }
+        }
+        _ => {
+            println!("cd: please provide only one directory");
+        }
+    }
+
+    Ok(())
+}
+
+fn resolve_path(ctx: &Context, path: &str) -> Result<PathBuf, anyhow::Error> {
+    let current_dir = &ctx.current_working_dir;
+
+    // Handle various path scenarios
+    let resolved_path = match path {
+        // Home directory variations
+        "" | "~" | "~/" => PathBuf::from(HOME),
+
+        // Home directory with subpath
+        path if path.starts_with("~/") => PathBuf::from(HOME).join(&path[2..]),
+
+        // Parent directory
+        ".." => current_dir
+            .parent()
+            .map(|p| p.to_path_buf())
+            .unwrap_or_else(|| current_dir.clone()),
+
+        // Relative paths starting with ../
+        path if path.starts_with("../") => {
+            let mut path_buf = current_dir.clone();
+            let remaining_path = &path[3..];
+
+            // Remove parent directories as needed
+            let _ = path_buf.pop();
+            path_buf.join(remaining_path)
+        }
+
+        // Relative paths starting with ./
+        path if path.starts_with("./") => current_dir.join(&path[2..]),
+
+        // Absolute paths
+        path if path.starts_with('/') => PathBuf::from(path),
+
+        // Relative paths
+        _ => current_dir.join(path),
+    };
+
+    match resolved_path.canonicalize() {
+        Ok(resolved) => Ok(resolved),
+        Err(_) => {
+            return Err(anyhow!("cd: {}: No such file or directory", path));
+        }
+    }
 }
 
 impl<'a> From<&'a [&'a str]> for ShellCommand<'a> {
@@ -53,195 +135,90 @@ const BUILTINS: [&str; 5] = ["echo", "exit", "type", "pwd", "cd"];
 const PATH: &'static str = env!("PATH");
 const HOME: &'static str = env!("HOME");
 
-fn get_executables(paths: &[&str], ctx: &mut Context) -> io::Result<()> {
+fn populate_executables(paths: &[&str], ctx: &mut Context) -> Result<(), anyhow::Error> {
     for path in paths {
-        let entries = fs::read_dir(path);
-
-        match entries {
-            Ok(entrs) => {
-                for entry in entrs {
-                    let entry = entry?;
-                    let path = entry.path();
-                    let file_name = entry.file_name().into_string().unwrap();
-                    ctx.executbles.insert(file_name, path);
+        if let Ok(entries) = fs::read_dir(path) {
+            for entry in entries.filter_map(Result::ok) {
+                let path = entry.path();
+                if let Some(file_name) = entry.file_name().to_str() {
+                    let _ = ctx.executables.insert(file_name.to_string(), path);
                 }
-            }
-            Err(err) => {
-                eprintln!("Error: {}", err)
             }
         }
     }
     Ok(())
 }
 
-fn eval_builtin(command: &str, args: &[&str], ctx: &mut Context) {
+fn eval_builtin(command: &str, args: &[&str], ctx: &mut Context) -> Result<(), anyhow::Error> {
     match command {
         "exit" => {
             let args = args[0].trim_end();
-            let exit_num = args.parse::<i32>().expect("This should be parsable");
+            let exit_num = args.parse::<i32>()?;
             exit(exit_num)
         }
         "echo" => {
             let line_to_print = args.join(" ").trim_end().to_owned();
             println!("{}", line_to_print);
+            Ok(())
         }
         "type" => {
             let cmd_to_check = args[0].trim_end();
             if BUILTINS.contains(&cmd_to_check) {
                 println!("{} is a shell builtin", cmd_to_check);
-            } else if ctx.executbles.contains_key(cmd_to_check) {
+                Ok(())
+            } else if ctx.executables.contains_key(cmd_to_check) {
                 println!(
                     "{} is {}",
                     cmd_to_check,
-                    ctx.executbles[cmd_to_check].display()
+                    ctx.executables[cmd_to_check].display()
                 );
+                Ok(())
             } else {
                 println!("{}: not found", cmd_to_check);
+                Ok(())
             }
         }
         "pwd" => {
-            let cwd = ctx.current_working_dir.to_str().unwrap();
+            let cwd = ctx
+                .current_working_dir
+                .to_str()
+                .ok_or_else(|| anyhow!("Option was none whoops!"))?;
             if cwd.ends_with("/") {
                 println!("{}", PathBuf::from(&cwd[0..cwd.len() - 1]).display())
             } else {
                 println!("{}", ctx.current_working_dir.display())
             }
+            Ok(())
         }
-        "cd" => {
-            if args.len() == 0 {
-                todo!("Should just change directory to home")
-            }
-            if args.len() > 1 {
-                println!("cd: please only provide one directory")
-            } else {
-                let path = args[0];
-                match PathBuf::from_str(path) {
-                    Ok(path) => {
-                        if path.is_absolute() {
-                            match fs::read_dir(&path) {
-                                Ok(_) => {
-                                    ctx.current_working_dir = path.clone();
-                                    std::env::set_current_dir(ctx.current_working_dir.clone())
-                                        .unwrap()
-                                }
-                                Err(_) => {
-                                    println!("cd: {}: No such file or directory", path.display())
-                                }
-                            }
-                        } else {
-                            if path == PathBuf::from_str("~/").unwrap() {
-                                if path.to_str().unwrap().len() > 1 {
-                                    ctx.current_working_dir =
-                                        PathBuf::from(HOME).join(&path.to_str().unwrap()[2..]);
-                                    std::env::set_current_dir(ctx.current_working_dir.clone())
-                                        .unwrap()
-                                } else {
-                                    ctx.current_working_dir =
-                                        PathBuf::from(HOME).join(&path.to_str().unwrap()[1..]);
-                                    std::env::set_current_dir(ctx.current_working_dir.clone())
-                                        .unwrap()
-                                }
-                            } else if path == PathBuf::from_str("..").unwrap() {
-                                ctx.current_working_dir.pop();
-                                std::env::set_current_dir(ctx.current_working_dir.clone()).unwrap()
-                            } else if path.starts_with("../") {
-                                ctx.current_working_dir.pop();
-                                let mut path_without_dots = &path.to_str().unwrap()[2..];
-                                if path_without_dots.ends_with("/") {
-                                    path_without_dots =
-                                        &path_without_dots[..path_without_dots.len() - 1]
-                                }
-                                let mut path_without_dots = PathBuf::from(path_without_dots);
-                                while path_without_dots.starts_with("/..") {
-                                    path_without_dots =
-                                        PathBuf::from(&path_without_dots.to_str().unwrap()[3..]);
-                                    ctx.current_working_dir.pop();
-                                }
-                                let mut total_path =
-                                    ctx.current_working_dir.join(path_without_dots);
-                                let total_path_str = total_path.to_str().unwrap();
-                                if total_path_str.ends_with("/") {
-                                    total_path =
-                                        PathBuf::from(&total_path_str[0..total_path_str.len() - 1]);
-                                }
-                                match fs::read_dir(&total_path) {
-                                    Ok(_) => {
-                                        ctx.current_working_dir = total_path.clone();
-                                        std::env::set_current_dir(ctx.current_working_dir.clone())
-                                            .unwrap()
-                                    }
-                                    Err(_) => {
-                                        println!(
-                                            "cd: {}: No such file or directory",
-                                            path.display()
-                                        )
-                                    }
-                                }
-                            } else if path.starts_with("./") {
-                                let path_without_dot = PathBuf::from(&path.to_str().unwrap()[2..]);
-                                let total_path = ctx.current_working_dir.join(path_without_dot);
-                                match fs::read_dir(&total_path) {
-                                    Ok(_) => {
-                                        ctx.current_working_dir = total_path.clone();
-                                        std::env::set_current_dir(ctx.current_working_dir.clone())
-                                            .unwrap()
-                                    }
-                                    Err(_) => {
-                                        println!(
-                                            "cd: {}: No such file or directory",
-                                            path.display()
-                                        )
-                                    }
-                                }
-                            } else {
-                                let path_without_dot = PathBuf::from(&path.to_str().unwrap()[2..]);
-                                let total_path = ctx.current_working_dir.join(path_without_dot);
-                                match fs::read_dir(&total_path) {
-                                    Ok(_) => {
-                                        ctx.current_working_dir = total_path.clone();
-                                        std::env::set_current_dir(ctx.current_working_dir.clone())
-                                            .unwrap()
-                                    }
-                                    Err(_) => {
-                                        println!(
-                                            "cd: {}: No such file or directory",
-                                            path.display()
-                                        )
-                                    }
-                                }
-                            }
-                        }
-                    }
-
-                    Err(err) => println!("ERROR: {err}"),
-                }
-            }
-        }
-        _ => {}
+        "cd" => change_directory(ctx, args),
+        _ => Ok(()),
     }
 }
 
-fn eval_executable(command: &str, args: &[&str], ctx: &Context) {
-    if ctx.executbles.contains_key(command) {
-        let full_path_cmd = ctx.executbles[command].to_str().unwrap();
+fn eval_executable(command: &str, args: &[&str], ctx: &Context) -> Result<(), anyhow::Error> {
+    if ctx.executables.contains_key(command) {
+        let full_path_cmd = ctx.executables[command].to_str().unwrap();
         let mut cmd = Command::new(full_path_cmd);
         let output = cmd.args(args).output().unwrap();
         // Check if the command was successful
         if output.status.success() {
             // Convert the output to a string and print it
-            let stdout = String::from_utf8(output.stdout).expect("Invalid UTF-8 sequence");
+            let stdout = String::from_utf8(output.stdout)?;
             print!("{}", stdout);
+            Ok(())
         } else {
             // If the command failed, print the error
-            let stderr = String::from_utf8(output.stderr).expect("Invalid UTF-8 sequence");
+            let stderr = String::from_utf8(output.stderr)?;
             eprintln!("Error:\n{}", stderr);
+            Ok(())
         }
     } else {
         println!("{}: command not found", command);
+        Ok(())
     }
 }
 
-fn eval(command: &str, ctx: &mut Context) {
+fn eval(command: &str, ctx: &mut Context) -> Result<(), anyhow::Error> {
     use ShellCommandType::*;
     let cmd_input = command
         .split(' ')
@@ -251,29 +228,32 @@ fn eval(command: &str, ctx: &mut Context) {
     let (cmd, args) = (shell_cmd.cmd, shell_cmd.args);
     match shell_cmd.command_type {
         Builtin => {
-            eval_builtin(cmd, args, ctx);
+            eval_builtin(cmd, args, ctx)?;
         }
         Executable => {
-            eval_executable(cmd, args, ctx);
+            eval_executable(cmd, args, ctx)?;
         }
     }
+    Ok(())
 }
 
 fn main() {
     // Wait for user input
     let mut ctx = Context {
-        executbles: HashMap::new(),
+        executables: HashMap::new(),
         current_working_dir: env::current_dir().expect("Shouldn't fail?"),
     };
     let paths = PATH.split(':').collect::<Vec<&str>>();
-    let _ = get_executables(&paths, &mut ctx);
+    let _ = populate_executables(&paths, &mut ctx);
     // dbg!(&ctx.executbles);
     let stdin = io::stdin();
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
         let mut command = String::new();
-        stdin.read_line(&mut command).unwrap();
-        eval(&command, &mut ctx);
+        let _ = stdin.read_line(&mut command).unwrap();
+        if let Err(e) = eval(&command, &mut ctx) {
+            eprintln!("{:?}", e);
+        }
     }
 }
